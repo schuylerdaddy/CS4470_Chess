64b4c02551d19b0aecfc4d0a4b578e94db3331cd
pixie1 <kjosien1@gmail.com>
2014-09-22 15:47:26 -0600

    Update Karine.cs

--- Karine.cs
+++ Karine.cs
@@ -1,1 +1,18 @@
-//insert code here
+public string convertToShallowRedForm (string originalFen)
+        {
+            string newForm="";
+            foreach (char c in originalFen ){
+                if (!char.IsDigit(c)) newForm += c;
+                else
+                {
+                    int count = Int32.Parse(c.ToString());
+                    for (int i = 0; i < count; ++i)
+                    {
+                        newForm += "_";
+                    }
+
+                }
+
+            }
+           return newForm;
+        }
